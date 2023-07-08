ebd18bda3da84a7f5ddeb9075a8bd7ccdb54a0ed
Octavia Togami <octavia.togami@gmail.com>
2023-07-07 23:05:33 -0700

    Update Gradle + deps

--- build-logic/build.gradle.kts
+++ build-logic/build.gradle.kts
@@ -12,7 +12,7 @@ repositories {
 dependencies {
     implementation(gradleApi())
     implementation("gradle.plugin.org.cadixdev.gradle:licenser:0.6.1")
-    implementation("org.jfrog.buildinfo:build-info-extractor-gradle:4.28.4")
+    implementation("org.jfrog.buildinfo:build-info-extractor-gradle:4.33.1")
 }
 
 configure<JavaPluginExtension> {
