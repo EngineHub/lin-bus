e233e0e7e1a5b5c69f7edd422d4c720ffa4c1375
Octavia Togami <octavia.togami@gmail.com>
2024-04-13 00:34:33 -0700

    Prepare for release

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,10 @@
 plugins {
     id("org.enginehub.lin-bus.publishing")
+    id("net.researchgate.release") version "3.0.2"
+}
+
+release {
+    tagTemplate = "v\${version}"
+    buildTasks = listOf<String>()
+    git.requireBranch = "master"
 }
