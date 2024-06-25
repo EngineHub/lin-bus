d81f223de882c5eee531152d7e5f49e1815296e7
Octavia Togami <octavia.togami@gmail.com>
2024-06-24 21:42:53 -0700

    Make a proper build step

---
+++ .teamcity/_Self/LinBus.kt
@@ -0,0 +1,38 @@
+package _Self
+
+import jetbrains.buildServer.configs.kotlin.BuildSteps
+import jetbrains.buildServer.configs.kotlin.BuildType
+import jetbrains.buildServer.configs.kotlin.DslContext
+import jetbrains.buildServer.configs.kotlin.RelativeId
+import jetbrains.buildServer.configs.kotlin.buildSteps.GradleBuildStep
+import jetbrains.buildServer.configs.kotlin.buildSteps.gradle
+import jetbrains.buildServer.configs.kotlin.triggers.vcs
+
+fun BuildSteps.configuredGradle(init: GradleBuildStep.() -> Unit) {
+    gradle {
+        enableStacktrace = true
+
+        init()
+    }
+}
+
+object Build : BuildType({
+    id = RelativeId("Build")
+    name = "Build"
+
+    vcs {
+        root(DslContext.settingsRoot)
+    }
+
+    triggers {
+        vcs {
+        }
+    }
+
+    steps {
+        configuredGradle {
+            name = "Build"
+            tasks = "clean build"
+        }
+    }
+})
--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,17 +1,8 @@
-import jetbrains.buildServer.configs.kotlin.*
-import jetbrains.buildServer.configs.kotlin.buildFeatures.perfmon
-import jetbrains.buildServer.configs.kotlin.triggers.vcs
+import _Self.Build
+import jetbrains.buildServer.configs.kotlin.project
+import jetbrains.buildServer.configs.kotlin.version
 
 /*
-The settings script is an entry point for defining a TeamCity
-project hierarchy. The script should contain a single call to the
-project() function with a Project instance or an init function as
-an argument.
-
-VcsRoots, BuildTypes, Templates, and subprojects can be
-registered inside the project using the vcsRoot(), buildType(),
-template(), and subProject() methods respectively.
-
 To debug settings scripts in command-line, run the
 
     mvnDebug org.jetbrains.teamcity:teamcity-configs-maven-plugin:generate
@@ -27,25 +18,5 @@ To debug in IntelliJ Idea, open the 'Maven Projects' tool window (View
 version = "2024.03"
 
 project {
-
-    buildType(LinBus_Build)
+    buildType(Build)
 }
-
-object LinBus_Build : BuildType({
-    id = AbsoluteId("LinBus_Build")
-    name = "Build"
-
-    vcs {
-        root(DslContext.settingsRoot)
-    }
-
-    triggers {
-        vcs {
-        }
-    }
-
-    features {
-        perfmon {
-        }
-    }
-})
