767c3b847a5c45cc0abfd9300cd3d8b1d1552220
Octavia Togami <octavia.togami@gmail.com>
2024-06-24 21:52:25 -0700

    Try adding commit status

--- .teamcity/_Self/LinBus.kt
+++ .teamcity/_Self/LinBus.kt
@@ -4,6 +4,7 @@ import jetbrains.buildServer.configs.kotlin.BuildSteps
 import jetbrains.buildServer.configs.kotlin.BuildType
 import jetbrains.buildServer.configs.kotlin.DslContext
 import jetbrains.buildServer.configs.kotlin.RelativeId
+import jetbrains.buildServer.configs.kotlin.buildFeatures.commitStatusPublisher
 import jetbrains.buildServer.configs.kotlin.buildSteps.GradleBuildStep
 import jetbrains.buildServer.configs.kotlin.buildSteps.gradle
 import jetbrains.buildServer.configs.kotlin.triggers.vcs
@@ -26,6 +27,15 @@ object Build : BuildType({
 
     triggers {
         vcs {
+        }
+    }
+
+    features {
+        commitStatusPublisher {
+            publisher = github {
+                githubUrl = "https://api.github.com"
+                authType = vcsRoot()
+            }
         }
     }
 
